0e08f072958db0ab0b738e9c61fb8f4114e5a79a
yojiyo <alcuizargiogio@gmail.com>
2026-04-16 22:57:55 +0800

    sqlite to postgresql

--- api/database.py
+++ api/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./gotrack.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://neondb_owner:npg_E4sgvuRDJeY7@ep-flat-darkness-anqggkab-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 
 # Fix Vercel Postgres URL format
 if DATABASE_URL.startswith("postgres://"):
